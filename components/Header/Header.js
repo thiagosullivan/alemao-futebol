3aca69fe653a1fa7c39183376f8310994712800b
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-08-25 04:41:32 -0300

    Header section

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import LogoHeader from '../../assets/alemao-logo.png';
+import { IoIosPin } from 'react-icons/io';
+import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
+
 import { HeaderContainer } from './style'
 
 function Header() {
   return (
     <HeaderContainer>
         <div className='top__header'>
-            <Link>
-                <Image />
-            </Link>
+            <div className='top__header__content'>
+                <div className='top__header__col1'>
+                    <IoIosPin />
+                    <p>Rua Sergipe, 800 - Centro - Londrina  - PR</p>
+                </div>
+                <div className='top__header__col2'>
+                    <p>Siga as nossas Redes Sociais:</p>
+                    <div className='top__header__sm'>
+                        <a href="facebook.com">
+                            <FaFacebookF />
+                        </a>
+                        <a href="instagram.com">
+                            <FaInstagram />
+                        </a>
+                        <a href="youtube.com">
+                            <FaYoutube />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='bottom__header'>
+            <div className='bottom__header__content'>
+                <Link href="/">
+                    <Image src={LogoHeader} width={61} height={65}/>
+                </Link>
+                <nav>
+                    <Link href="/">Home</Link>
+                    <Link href="/">Quem Somos</Link>
+                    <Link href="/">Unidades</Link>
+                    <Link href="/">Alunos</Link>
+                    <Link href="/">Licenciados</Link>
+                    <Link href="/">Parceiros</Link>
+                </nav>
+            </div>
         </div>
     </HeaderContainer>
   )
--- components/Header/style.js
+++ components/Header/style.js
@@ -2,4 +2,90 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
 
+    .top__header {
+        background-color: ${({theme}) => theme.secondary};
+        color: ${({theme}) => theme.white};
+        
+        .top__header__content {
+            max-width: 1170px;
+            width: 100%;
+            margin: 0 auto;
+            padding: .5rem 1rem;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .top__header__col1 {
+                display: flex;
+                align-items: center;
+                
+                p {
+                    font-family: 'Lato', sans-serif;
+                }
+
+                svg {
+                    &:first-child {
+                        margin-right: 10px;
+                    }
+                }
+            }
+            .top__header__col2 {
+                display: flex;
+                align-items: center;
+
+                p {
+                    margin-right: 10px;
+                    font-family: 'Lato', sans-serif;
+                }
+
+                .top__header__sm {
+                    display: flex;
+                    align-items: center;
+
+                    a {
+                        width: 32px;
+                        height: 32px;
+                        background-color: ${({theme}) => theme.white};
+                        border-radius: 50px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        margin: 0 5px;
+
+                        svg {
+                            color: ${({theme}) => theme.secondary};
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    .bottom__header {
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+        .bottom__header__content {
+            max-width: 1170px;
+            width: 100%;
+            margin: 0 auto;
+            padding: 1rem;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            nav {
+                a {
+                    font-family: 'Raleway', sans-serif;
+                    font-weight: 700;
+                    text-transform: uppercase;
+                    margin-right: 40px;
+                    color: ${({theme}) => theme.primary};
+
+                    &:last-child {
+                        margin-right: 0;
+                    }
+                }
+            }
+        }
+    }
 `
