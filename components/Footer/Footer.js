d2d3b6240822754029b154b629afa45fb6b36e49
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-08-25 03:41:55 -0300

    Adding footer

---
+++ components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterContainer } from './style'
+
+function Footer() {
+  return (
+    <FooterContainer>
+        Footer
+    </FooterContainer>
+  )
+}
+
+export default Footer;
