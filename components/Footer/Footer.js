df35cdc56323c27de8384aaf0eca6e7c8f4d0994
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-08-29 12:15:52 -0300

    Footer section

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,72 @@
-import React from 'react'
-import { FooterContainer } from './style'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import LogoFooter from '../../assets/alemao-logo-white.png';
+import { FaFacebookF, FaInstagram, FaYoutube, FaTiktok } from 'react-icons/fa';
+import { FooterContainer } from './style';
 
 function Footer() {
   return (
     <FooterContainer>
-        Footer
+        <div className='top__footer'>
+          <Link href="/">
+            <Image src={LogoFooter} width={143} height={150}/>
+          </Link>
+          <nav>
+            <Link href="/">
+              Quem Somos
+            </Link>
+            <Link href="/">
+              Notícias & Ações
+            </Link>
+            <Link href="/">
+              Destaques das unidades
+            </Link>
+            <Link href="/">
+              Unidades
+            </Link>
+            <Link href="/">
+              Metodologia
+            </Link>
+            <Link href="/">
+              Alunos
+            </Link>
+            <Link href="/">
+              Licenciados
+            </Link>
+            <Link href="/">
+              Parceiros
+            </Link>
+            <Link href="/">
+              Materiais Oficiais
+            </Link>
+            <Link href="/">
+              Revista Chute Inicial
+            </Link>
+            <Link href="/">
+              Newsletter
+            </Link>
+          </nav>
+          <p>Siga nossas redes sociais:</p>
+          <div className='footer__sm'>
+            <a href="https://www.facebook.com/alemaofutebol" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF />
+            </a>
+            <a href="https://www.instagram.com/alemao_treinador" et="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+            <a href="https://www.youtube.com/channel/UC5g0_uQsY0cA0tFdCBQpGEA" target="_blank" rel="noopener noreferrer">
+              <FaYoutube />
+            </a>
+            <a href="https://www.tiktok.com/@alemaofutebol" target="_blank" rel="noopener noreferrer">
+              <FaTiktok />
+            </a>
+          </div>
+          <span>Alemao training LTDA CNPJ: 47.628.685/0001-89 © 2022 - Todos os direitos reservados</span>
+        </div>
+        <div className='bottom__footer'>
+          <p>Desenvolvido por <a href='#' target="_blank" rel="noopener noreferrer"><strong>Digital Cloud Marketing</strong></a></p>
+        </div>
     </FooterContainer>
   )
 }
--- components/Footer/style.js
+++ components/Footer/style.js
@@ -1,5 +1,97 @@
+import { darken, lighten } from "polished";
 import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
+    margin-top: 5rem;
+    color: ${({theme}) => theme.white};
+    
+    .top__footer {
+        background-image: url(https://raw.githubusercontent.com/thiagosullivan/alemao-futebol/main/assets/footer-bg.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        padding: 5rem 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
 
+        nav {
+            border-top: 3px solid ${({theme}) => theme.white};
+            border-bottom: 3px solid ${({theme}) => theme.white};
+            padding: 2rem;
+            max-width: 1170px;
+            width: 100%;
+            margin: 3rem auto;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+
+            a {
+                font-family: 'Raleway';
+                font-size: 1rem;
+                font-weight: bold;
+                text-transform: uppercase;
+                margin: 20px;
+
+                &:hover {
+                    color: ${({ theme }) => darken(0.3, theme.white)};
+                }
+            }
+        }
+
+        p {
+            font-family: 'Raleway';
+            font-size: 1rem;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 2rem;
+        }
+
+        span {
+            font-family: 'Raleway';
+            font-size: 0.75rem;
+            text-transform: uppercase;
+        }
+
+        .footer__sm {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-bottom: 3rem;
+
+            a {
+                background-color: ${({theme}) => theme.white};
+                width: 45px;
+                height: 45px;
+                font-size: 2rem;
+                border-radius: 35px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: ${({theme}) => theme.secondary};
+                margin: 0 .5rem;
+                transition: all 100ms ease-in;
+
+                &:hover {
+                    background-color: ${({theme}) => theme.secondary};
+                    color: ${({theme}) => theme.white};
+                }
+            }
+        }
+    }
+    .bottom__footer {
+        color: ${({theme}) => theme.secondary};
+        text-align: center;
+        padding: .5rem 0;
+        
+        p {
+            font-family: 'Raleway';
+
+            a {
+                &:hover {
+                    color: ${({ theme }) => darken(0.5, theme.white)};
+                }
+            }
+        }
+    }
 `;
